6ecdc9e2fac77320a2d7a56ca19a844b4124a7c7
Bartosz Zurkowski <bartosz.zurkowski@codesthq.com>
2016-10-15 18:03:38 +0200

    Add router

--- src/js/App.js
+++ src/js/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router'
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
         <h1>This is so cool!</h1>
+        {this.props.children}
+        <Link to="todos">Todos</Link>
+        <Link to="settings">Settings</Link>
       </div>
     );
   }
--- src/js/client.js
+++ src/js/client.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from "react-router"
 
-import App from "./App";
+import App      from "./App";
+import Todos    from "./pages/Todos";
+import Settings from "./pages/Settings";
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const app = document.getElementById('app');
+
+ReactDOM.render(
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Todos}></IndexRoute>
+      <Route path="todos" component={Todos}></Route>
+      <Route path="settings" component={Settings}></Route>
+    </Route>
+  </Router>,
+  app
+);
